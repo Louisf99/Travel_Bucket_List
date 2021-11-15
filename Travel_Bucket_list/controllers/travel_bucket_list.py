0e25ca5ebc9ae4fe2727036843e5a78eeb24b278
Louis Fletcher <Louisco1999@gmail.com>
2021-11-15 14:40:20 +0000

    removed tests as no longer needed and  reated error when adding user to country class

--- Travel_Bucket_list/controllers/travel_bucket_list.py
+++ Travel_Bucket_list/controllers/travel_bucket_list.py
@@ -2,4 +2,4 @@ from flask import Flask, render_template, request, redirect
 from models.country import Country
 from flask import Blueprint
 from repositories import country_repository, user_repository
-tasks_blueprint = Blueprint("", __name__)
+tasks_blueprint = Blueprint("countries", __name__)
--- Travel_Bucket_list/run_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest 
-from class_tests.country_test import TestCountry
-from class_tests.user_test import TestUser
-
-
-if __name__ == "__main__":
-    unittest.main()
