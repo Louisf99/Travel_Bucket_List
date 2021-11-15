d31b962c41c19c956843d530af8718e2647b8c8b
Louis Fletcher <Louisco1999@gmail.com>
2021-11-15 11:08:41 +0000

    changed Country class to be more basic

--- Travel_Bucket_list/models/country.py
+++ Travel_Bucket_list/models/country.py
@@ -1,11 +1,12 @@
 class Country:
 
-    def __init__(self, name, population, currency, language, visited = False, id = None):
+    def __init__(self, name, city, population, currency, language, visited = False, id = None):
         self.name = name
+        self.city = city
         self.population = population
         self.currency = currency
         self.language = language
         self.visited = visited
         self.id = id
-        self.cities = []
+        
 
