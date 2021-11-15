010986c19da9f8698d3691d15209273c196d3400
Louis Fletcher <Louisco1999@gmail.com>
2021-11-15 15:41:15 +0000

    added save function to country repo

--- Travel_Bucket_list/repositories/country_repository.py
+++ Travel_Bucket_list/repositories/country_repository.py
@@ -2,4 +2,10 @@ from db.run_sql import run_sql
 from models.country import Country
 from models.city import City
 
-
+def save(country):
+    sql = "INSERT INTO countries (name) VALUES (%s) RETURNING *"
+    values = [country.name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    country.id = id
+    return country
