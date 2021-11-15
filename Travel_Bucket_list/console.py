6e9c60e72d468463473aed7e97bc84e0d83f5641
Louis Fletcher <Louisco1999@gmail.com>
2021-11-15 14:43:51 +0000

    added console boiler plate

---
+++ Travel_Bucket_list/console.py
@@ -0,0 +1,6 @@
+import pdb
+from models.country import Country
+from models.user import User
+
+import repositories.country_repository as task_repository
+import repositories.user_repository as user_repository
