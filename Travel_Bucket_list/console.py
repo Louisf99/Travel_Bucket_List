1f9b43518c500ff4ff08c561a9def1e64052670a
Louis Fletcher <Louisco1999@gmail.com>
2021-11-15 22:36:45 +0000

    ran console.py the tables are linked correctly but the country table is not populating all fields

--- Travel_Bucket_list/console.py
+++ Travel_Bucket_list/console.py
@@ -18,8 +18,8 @@ country_repository.save(country3)
 country4 = Country("France", 67000000, "Euro", "French")
 country_repository.save(country4)
 
-country3.language = "French"
-country_repository.update(country3)
+# country3.language = "French"
+# country_repository.update(country3)
 
 
 # Test City repo fucntionality
@@ -28,8 +28,8 @@ city1 = City("London", country1)
 city_repository.save(city1)
 
 city2 = City("Edinburgh", country2, True)
-city_repository.save(country2)
+city_repository.save(city2)
 
-city_repository.delete(1)
+# city_repository.delete(1)
 
 pdb.set_trace()
