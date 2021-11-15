b3d5e70fc8372dc29e17e58da9682e97316052fa
Louis Fletcher <Louisco1999@gmail.com>
2021-11-15 15:59:00 +0000

    added boilerplate to console

--- Travel_Bucket_list/console.py
+++ Travel_Bucket_list/console.py
@@ -1,1 +1,6 @@
+import pdb
+from models.city import City
+from models.country import Country
 
+import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
