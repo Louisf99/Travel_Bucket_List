4cb4457500725f6880623e5dd37f96626d538320
Louis Fletcher <Louisco1999@gmail.com>
2021-11-15 11:15:32 +0000

    added User class with attributes

---
+++ Travel_Bucket_list/models/user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, name, age, favourite_country id = None):
+        self.name = name
+        self.age = age
+        self.favourite_country = favourite_country
+        self.id = id
