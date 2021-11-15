9f7c0b1c84fe8ed7f692e56d1e4de07cdc0f8b7d
Louis Fletcher <Louisco1999@gmail.com>
2021-11-15 11:49:10 +0000

    addded file to run tests for class attributes

--- Travel_Bucket_list/class_tests/user_test.py
+++ Travel_Bucket_list/class_tests/user_test.py
@@ -3,4 +3,9 @@ from models.user import User
 
 class TestCountry(unittest.TestCase):
     def setUp(self):
-        self.user = User()
+        self.user = User("Ben Cooney", 22, "Scotland")
+
+    def test_user_has_name(self):
+        self.assertEqual("Ben Cooney", self.customer.name)
+
+    
---
+++ Travel_Bucket_list/run_tests.py
@@ -0,0 +1,7 @@
+import unittest 
+from class_tests.country_test import TestCountry
+from class_tests.user_test import TestUser
+
+
+if __name__ == "__main__":
+    unittest.main()
