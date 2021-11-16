381258b08ea4272e3dc4fdab81384207ca6cd0dd
Louis Fletcher <Louisco1999@gmail.com>
2021-11-16 01:14:50 +0000

    added comments to highlight where tests are and where functions should be

--- Travel_Bucket_list/console.py
+++ Travel_Bucket_list/console.py
@@ -5,7 +5,9 @@ from models.country import Country
 import repositories.city_repository as city_repository
 import repositories.country_repository as country_repository
 
+# Testing CRUD functionality for country repo
 
+# Test CREATE
 country1 = Country("England", 58000000, "GBP", "English")
 country_repository.save(country1)
 
@@ -18,15 +20,24 @@ country_repository.save(country3)
 country4 = Country("France", 67000000, "Euro", "French")
 country_repository.save(country4)
 
+# Test READ
+# Test UPDATE
+# Test DELETE
 
 # Test City repo fucntionality
 
+# Test CREATE
 city1 = City("London", country1)
 city_repository.save(city1)
 
 city2 = City("Edinburgh", country2, True)
 city_repository.save(city2)
 
+# Test READ
+
+# Test UPDATE
+
+# Test DELETE
 city_repository.delete(1)
 
 pdb.set_trace()
