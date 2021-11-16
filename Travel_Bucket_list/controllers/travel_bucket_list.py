543ea37726632401235ece6d3b26d1843342dc77
Louis Fletcher <Louisco1999@gmail.com>
2021-11-16 15:35:07 +0000

    created and edited html files and also added in delete and add for city and alot more changes

--- Travel_Bucket_list/controllers/travel_bucket_list.py
+++ Travel_Bucket_list/controllers/travel_bucket_list.py
@@ -12,14 +12,14 @@ cities_blueprint = Blueprint("cities", __name__)
 def destinations():
     cities = city_repository.select_all()
     print(cities)
-    return render_template("cities/index.html", cities=cities)
+    return render_template('cities/index.html', cities=cities)
 
-@cities_blueprint.route("/cities/visited", methods=['GET'])
+@cities_blueprint.route('/cities/visited', methods=['GET'])
 def visited_cities():
     all_visited_cities = city_repository.select_all_visited()
-    return render_template("/cities/visited.html", all_visited_cities = all_visited_cities)
+    return render_template('/cities/visited.html', all_visited_cities = all_visited_cities)
 
-@cities_blueprint.route("/cities/not_visited", methods=['GET'])
+@cities_blueprint.route('/cities/not_visited', methods=['GET'])
 def not_visited_cities():
     not_visited_cities = city_repository.select_all_not_visited()
     return render_template("/cities/not_visited.html", not_visited_cities = not_visited_cities)
@@ -27,4 +27,19 @@ def not_visited_cities():
 @cities_blueprint.route('/cities/<id>/delete', methods=['POST'])
 def delete_city(id):
     city_repository.delete(id)
+    return redirect('/cities')
+
+@cities_blueprint.route('/cities/new_destination', methods=['GET'])
+def new_task():
+    countries = country_repository.select_all()
+    return render_template('cities/add_city.html', all_countries=countries)
+
+@cities_blueprint.route('/cities', methods=['POST'])
+def create_city():
+    name = request.form['name']
+    country_id = request.form['country_id']
+    visited = request.form['visited']
+    country = country_repository.select(country_id)
+    city = City(name, country, visited)
+    city_repository.save(city)
     return redirect('/cities')
